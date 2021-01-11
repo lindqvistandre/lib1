f41419e9e922fa1d48a75479aad5230004850231
André Lindqvist <andre.lindqvist82@gmail.com>
2021-01-11 13:29:26 +0100

    innan du korrigerar din medlem kontroller.

--- lib1/Controllers/MembersController.cs
+++ lib1/Controllers/MembersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Lib1.Data;
-using Lib1.Models;
+using Lib1.Models;     // customer = member   film = book       actor = author 
 
 namespace lib1.Controllers
 {
